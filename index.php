f6adeb89c7d8f3a4d9cb67f4e1abbc2b3c17a0d9
Mario Lopriore <mariojames211@gmail.com>
2018-09-06 09:38:15 -0700

    Initial code commit - just starter content and basic file structure

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Cascadia Piper | John Goff" />
+    <meta name="description" content="Cascadia Piper bagpipe services include: parties, weddings, funerals, wakes, and other special events. Bagpipes provide a traditional, respectful, and honorable way to cherish past loved ones and welcome new special family and friends. (503) 960-5599" />
+    <meta name="keywords" content="portland bagpiper, portland bagpipe player, bagpipes in portland oregon, bagpipes in vancouver washington, portland bagpipes, bagpipes" />
+
+		<title>Cascadia Piper</title>
+
+    <!-- Facebook Open Graph markup -->
+    <meta property="og:url" content="https://www.cascadiapiper.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="John Goff: The Cascadia Piper" />
+    <meta property="og:description" content="Award winning bagpiper in Portland Oregon. John Goff, the Cascadia Piper plays babpipes for parties, weddings, funerals, wakes, and special occasions." />
+    <meta property="og:image" content="" />
+    <meta property="fb:app_id" content="2131529450393664" />
+
+    <!-- Twitter Card markup -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="" />
+    <meta name="twitter:title" content="Cascadia Piper | Bagpiper in Portland Oregon" />
+    <meta name="twitter:description" content="Award winning bagpiper in Portland Oregon. John Goff, the Cascadia Piper plays babpipes for parties, weddings, funerals, wakes, and special occasions." />
+    <meta name="twitter:image" content="" />
+
+    <!-- Stylesheets in the css folder -->
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- Font Awesome CDN -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+
+    <!-- Facebook feed script -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=2131529450393664&autoLogAppEvents=1';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <main>
+      <h1>Bagpiper For Hire: Portland, OR & Vancouver, WA</h1>
+      <p>John Goff – the Cascadia Piper, a second-generation bagpiper has performed at hundreds of weddings, funerals, celebrations and parties. As a competitive bagpiper since 1984, John has won numerous prizes in bagpipe band and solo competitions. John is one of the most prolific pipers in the Portland area playing 82 times last year alone at events throughout the west coast from Vancouver BC to California’s Bay Area. John’s passion for performing shows in his exacting attitude to make every performance/event as perfect as humanly possible. John prides himself on free and clear communication with every person that contacts him and will always entertain any idea for an event.</p>
+      <div class="fb-page" data-href="https://www.facebook.com/cascadiapiper/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/cascadiapiper/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/cascadiapiper/">Cascadia Piper</a></blockquote></div>
+      <h2>Cascadia Piper’s Services:</h2>
+      <h3>Bagpiper Services by Cascadia Piper</h3>
+      <p>John Goff, the Cascadia Piper offers a variety of services for the community and individuals throughout the Pacific Northwest. His knowledge of traditional music and dress provide authenticity while his background of award winning performances, second-generation bagpiping experience, and integrity will enhance your special event. Special musical requests can be honored by arrangement.</p>
+
+      <h3>Bagpiper for Weddings</h3>
+      <p>Bagpipes are a festive addition to your special day. Many people enjoy having piping greet guests as they arrive for the event. John can pipe you in for the procession, play during the ceremony, or as you depart for the reception. John has played scores of weddings – big and small – in all settings: from churches and historic sites to vineyards, boats, beaches, mountainsides and parks, and can offer ideas and song suggestions.</p>
+
+      <h3>Bagpiper for Funerals</h3>
+      <p>John has played bagpipes at hundreds of funerals, memorial services, and celebrations of life. He also has vast experience playing military funerals with honors. John has played in a variety of settings, from graveside services to churches, and places in nature. He will work with you to create the respectful and honorable experience you desire.</p>
+
+      <h3>Bagpiper for Special Events<h3>
+      <p>Bagpipes are a wonderful way to celebrate! Great for birthday parties, corporate parties, surprise parties, anniversary events, bachelor/bachelorette parties, cultural events, St. Patrick’s Day, parades, and countless other occasions. Contact John to customize your special event.</p>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+	</body>
+</html>
