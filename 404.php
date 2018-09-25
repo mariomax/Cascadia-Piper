993b124aebf620793fbf0be729668a0deac9fc07
Mario Lopriore <mariojames211@gmail.com>
2018-09-24 18:53:53 -0700

    New images.

---
+++ 404.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Cascadia Piper | John Goff" />
+    <meta name="description" content="Cascadia Piper bagpipe services include: parties, weddings, funerals, wakes, and other special events. Bagpipes provide a traditional, respectful, and honorable way to cherish past loved ones and welcome new special family and friends. (503) 960-5599" />
+    <meta name="keywords" content="portland bagpiper, portland bagpipe player, bagpipes in portland oregon, bagpipes in vancouver washington, portland bagpipes, bagpipes" />
+
+		<title>Cascadia Piper</title>
+
+    <!-- Stylesheets in the css folder -->
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- jQuery script -->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+    <!-- Font Awesome CDN -->
+		<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+		
+	<body>
+    
+    <?php include 'includes/header.php'; ?>
+    
+    <main>
+      <section class="four0four">
+        <h1>Cascadia Piper</h1>
+        <h2>Error!</h2>
+        <p><i class="far fa-frown"></i>&nbsp; Sorry. The page you were looking for isn't available or the link to it is broken.</p>
+      <button class="return"><a href="https://www.cascadiapiper.com/">Return to the site &gt; &gt;</a></button>
+      </section>
+    </main>
+
+    <?php include 'includes/footer.php'; ?>
+    
+    <!-- JavaScript file from js folder -->
+    <script src="../js/main.js" type="text/javascript"></script>
+    <script src="//cdn.jsdelivr.net/jquery.lazyloadxt/1.0.0/jquery.lazyloadxt.min.js"></script>
+
+  </body>
+</html> 
